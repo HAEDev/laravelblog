3cb41335e17598119e27e2b5ed81cafa9b40a278
Adam Smith <adam.smith@haegroup.com>
2020-05-07 11:06:19 +0100

    Updated file index

--- src/Models/BlogFile.php
+++ src/Models/BlogFile.php
@@ -14,4 +14,34 @@ class BlogFile extends BlogModel
     ];
 
     public $table = "blog_files";
+
+    /**
+     * Returns the full path to the location the file is stored.
+     *
+     * @return string
+     */
+    public function getPath()
+    {
+        if ($this->storage_location == "storage")
+        {
+            return public_path("storage/".config("laravel-blog.files.storage_path") . "/" . $this->path);
+        }
+
+        return public_path(config("laravel-blog.files.storage_path") . '/' . $this->path);
+    }
+
+    /**
+     * Returns the URL of the file.
+     *
+     * @return \Illuminate\Contracts\Routing\UrlGenerator|string
+     */
+    public function getUrl()
+    {
+        if ($this->storage_location == "storage")
+        {
+            return url("storage/".config("laravel-blog.files.storage_path") . "/" . $this->path);
+        }
+
+        return url(config("laravel-blog.files.storage_path") . "/" . $this->path);
+    }
 }
--- src/Views/files/index.blade.php
+++ src/Views/files/index.blade.php
@@ -31,78 +31,55 @@
 
             @include("laravel-blog::actions")
 
-            <div class="files-flex">
-                @forelse($files as $file)
-                    <div class="file">
-
-                        @if(Request::get("laravel-blog-embed", false))
-                            <a href="{{ $file->getUrl() }}" target="_blank" class="ck-select-file"
-                               data-url="{{ $file->getUrl() }}" data-alt-text="{{ $file->alt_text }}">
-                                <img src="{{ $file->getUrl() }}" alt="">
-                            </a>
-                        @else
-                            <a href="{{ $file->getUrl() }}" target="_blank">
-                                <img src="{{ $file->getUrl() }}" alt="">
-                            </a>
-                        @endif
-
-                        @if(Request::get("laravel-blog-featured", false))
-                            <div class="actions text-center">
-                                <button class="btn btn-xs btn-primary select-featured" data-id="{{ $file->id }}"
-                                    data-url="{{ $file->getUrl() }}" data-alt-text="{{ $file->alt_text }}">
-                                    Select
-                                </button>
-                            </div> <!-- End .actions.text-center -->
-                        @elseif(Request::get("laravel-blog-embed", false))
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($files as $file)
+                    <tr>
+                        <td><a href="{{ $file->getUrl() }}" target="_blank">{{ $file->path }}</a></td>
+                        <td>
+                            @if(Request::get("laravel-blog-embed", false))
                             <div class="actions text-center">
                                 <button class="btn btn-xs btn-primary ck-select-file" data-id="{{ $file->id }}"
                                         data-url="{{ $file->getUrl() }}" data-alt-text="{{ $file->alt_text }}">
                                     Select
                                 </button>
                             </div> <!-- End .actions.text-center -->
-                        @else
-                            <div class="actions text-right">
-                                <div class="dropdown">
-                                    <button class="btn btn-default btn-xs dropdown-toggle"
-                                            type="button" id="dropdownMenu-{{$file->id}}"
-                                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-                                        Actions
-                                        <span class="caret"></span>
-                                    </button>
-                                    <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu-{{$file->id}}">
-                                        <li>
-                                            <a href="{{ $file->getUrl() }}" target="_blank">
-                                                <i class="fa fa-fw fa-eye"></i>View
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="{{ blogUrl("files/$file->id/edit") }}">
-                                                <i class="fa fa-fw fa-pencil"></i>Edit
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="#" class="copy-url"><i class="fa fa-fw fa-copy"></i>Copy Url</a>
-                                            <input type="text" value="{{ $file->getUrl() }}" />
-                                        </li>
-                                        <li role="separator" class="divider"></li>
-                                        <li>
-                                            <form action="{{ blogUrl("files/$file->id") }}" method="post" class="form-inline confirm-delete">
-                                                {{ csrf_field() }} {{ method_field("DELETE") }}
-                                                <button class="dropdown-button"><i class="fa fa-fw fa-trash"></i>Delete</button>
-                                            </form>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div> <!-- End .actions.text-right -->
-                        @endif
-
-                    </div>
-                @empty
-                    <div class="text-muted">
-                        No files uploaded yet
-                    </div>
-                @endforelse
-            </div>
+                            @else
+                                <div class="actions text-right">
+                                    <div class="dropdown">
+                                        <button class="btn btn-default btn-xs dropdown-toggle"
+                                                type="button" id="dropdownMenu-{{$file->id}}"
+                                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+                                            Actions
+                                            <span class="caret"></span>
+                                        </button>
+                                        <ul class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenu-{{$file->id}}">
+                                            <li>
+                                                <a href="#" class="copy-url"><i class="fa fa-fw fa-copy"></i>Copy Url</a>
+                                                <input type="text" value="{{ $file->getUrl() }}" />
+                                            </li>
+                                            <li role="separator" class="divider"></li>
+                                            <li>
+                                                <form action="{{ blogUrl("files/$file->id") }}" method="post" class="form-inline confirm-delete">
+                                                    {{ csrf_field() }} {{ method_field("DELETE") }}
+                                                    <button class="dropdown-button"><i class="fa fa-fw fa-trash"></i>Delete</button>
+                                                </form>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div> <!-- End .actions.text-right -->
+                            @endif
+                        </td>
+                    </tr>
+                </tbody>
+                @endforeach
+            </table>
 
             @if($files)
                 <div class="text-right">
